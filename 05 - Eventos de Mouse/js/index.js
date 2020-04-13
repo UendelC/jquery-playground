3c2400f4d025bf948deaa42bf48e227a2ffddedb
Uendel Couto <uendel.couto@gmail.com>
2020-04-13 09:23:45 -0300

    Movimentos do mouse

--- 05 - Eventos de Mouse/js/index.js
+++ 05 - Eventos de Mouse/js/index.js
@@ -23,4 +23,36 @@ $(function () {
         $(this).css('background', '#ccc');
         ex.text('Você passou o mouse');
     });
+
+    $('.ev5').mousedown(function () {
+        $(this).css('background', '#ccc');
+        ex.text('Você apertou o mouse');
+
+    }).mouseup(function () {
+        $(this).css('background', '#000');
+        ex.text('você soltou o mouse');
+    });
+
+    var a = 0;
+    $('.ev6').mouseenter(function () {
+        a++;
+        ex.text('Entradas do Mouse ' + a);
+    }).mouseout(function () {
+        ex.text('Saida do mouse');
+    });
+    var b = 0;
+
+    $('.ev7').mouseover(function () {
+        b++;
+        ex.text('Mouse over: ' + b);
+    }).mouseleave(function () {
+        ex.text('Mouse leave');
+    });
+
+    $('.ev8').mousemove(function (move) {
+        var localx = move.pageX;
+        var localy = move.pageY;
+
+        ex.text('Movimento X: ' + localx + 'Movimento Y: ' + localy);
+    });
 });
