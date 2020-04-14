3bf388ad5b2bda8549de0eeeb64294c8a2a6573d
Uendel Couto <uendel.couto@gmail.com>
2020-04-14 16:16:43 -0300

    Menu Fixo

---
+++ 10 - Menu Fixo/js/index.js
@@ -0,0 +1,11 @@
+$(function () {
+    let nav = $('.menu');
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 173) {
+            nav.addClass('menu-fixo');
+        } else {
+            nav.removeClass('menu-fixo');
+        }
+    });
+});
