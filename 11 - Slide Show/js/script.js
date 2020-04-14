bdf314c45fbfc2dd24d0f4d648702d0658433e40
Uendel Couto <uendel.couto@gmail.com>
2020-04-14 16:56:25 -0300

    Slide Show

---
+++ 11 - Slide Show/js/script.js
@@ -0,0 +1,34 @@
+$(function () {
+    $('#slide img:eq(0)')
+        .addClass('ativo')
+        .show();
+    let texto = $('.ativo').attr('alt');
+    $('#slide').prepend('<p>' + texto + '</p>');
+    setInterval(slide, 3000);
+
+    function slide() {
+        if ($('.ativo').next().length) {
+            $('.ativo')
+                .fadeOut()
+                .removeClass('ativo')
+                .next()
+                .fadeIn()
+                .addClass('ativo');
+        } else {
+            $('.ativo')
+                .fadeOut()
+                .removeClass('ativo');
+            $('#slide img:eq(0)')
+                .fadeIn()
+                .addClass('ativo');
+        }
+
+        let texto = $('.ativo').attr('alt');
+        $('#slide p')
+            .hide()
+            .html(texto)
+            .delay(500)
+            .fadeIn();
+    }
+
+});
